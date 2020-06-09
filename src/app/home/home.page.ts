3664bb1c378826503701652039af12f9df2e62fc
Leonardo Nunes Soares <leonunessoares@hotmail.com>
2020-06-08 23:48:08 -0300

    Atualizações

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,47 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  scannedData: any;
+  validaDataScan: any = "0";
+  //encodedData: '';
+  //encodeData: any;
 
-  constructor() {}
+  constructor(public barcodeCtrl: BarcodeScanner) {}
 
-}
+ 
+
+  lerCodigo() {
+      const options: BarcodeScannerOptions = {
+        preferFrontCamera: false,
+        showFlipCameraButton: true,
+        showTorchButton: true,
+        
+        torchOn: false,
+        prompt: 'Coloque o código dentro da área',
+        resultDisplayDuration: 400,
+        formats: 'QR_CODE,PDF_417,EAN_13',
+        orientation: 'landscape',
+      };
+  
+      this.barcodeCtrl.scan(options).then(barcodeData => {
+        //alert('Barcode data' +  barcodeData);
+        //alert(barcodeData.text);
+        var str = barcodeData.text;
+        this.validaDataScan = "0";
+        if(str.match(/http/)){
+         // alert('string encontrada');
+          this.validaDataScan = "1";
+          //alert(this.validaDataScan);
+        
+        }
+        this.scannedData = barcodeData;
+  
+      }).catch(err => {
+        alert('Error' +  err);
+      });
+    }
+  
+  
+    
+  
+  }
